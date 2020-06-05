858a387d9966c330890e01e28b288a019dcdbfb3
RaynoldTanYX <raynoldtanyx@gmail.com>
2020-06-05 11:01:35 +0800

    Updates

---
+++ precache-manifest.45657dc33b4c709e0c572c1bcb05a167.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb2a897f182cd2dff4f8babe4444bb2",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "0030d5d470bd98fd581b",
+    "url": "/my-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8608c5c38e576009d9fb",
+    "url": "/my-website/static/js/2.23b04193.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-website/static/js/2.23b04193.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0030d5d470bd98fd581b",
+    "url": "/my-website/static/js/main.3a485835.chunk.js"
+  },
+  {
+    "revision": "dc2e3b3511453d8cdedf",
+    "url": "/my-website/static/js/runtime-main.dac0fb3d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
