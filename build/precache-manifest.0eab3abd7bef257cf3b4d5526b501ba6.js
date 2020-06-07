364ec57a60b88f7164d87820951e8f4047a21341
RaynoldTanYX <raynoldtanyx@gmail.com>
2020-06-07 17:36:02 +0800

    adding build folder

---
+++ build/precache-manifest.0eab3abd7bef257cf3b4d5526b501ba6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba5d0a259b259cae6d69a98b048bb1c",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "5e3503d1e63496808384",
+    "url": "/my-website/static/css/main.248721e0.chunk.css"
+  },
+  {
+    "revision": "400b8900e78f69ecb31b",
+    "url": "/my-website/static/js/2.81c8e8bc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-website/static/js/2.81c8e8bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e3503d1e63496808384",
+    "url": "/my-website/static/js/main.cee07074.chunk.js"
+  },
+  {
+    "revision": "dc2e3b3511453d8cdedf",
+    "url": "/my-website/static/js/runtime-main.dac0fb3d.js"
+  },
+  {
+    "revision": "4523c133a707d369dec4d117455e2468",
+    "url": "/my-website/static/media/header.4523c133.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
